4f4468072c34dd29b6604a9c37e81ba94b56529c
claudia ciontu <ciontu.claudia.a6e@student.ucv.ro>
2023-03-12 16:31:27 +0200

    chore: added try catch message

--- src/run/Main.java
+++ src/run/Main.java
@@ -6,10 +6,13 @@ import java.awt.GridLayout;
 
 import javax.swing.SwingUtilities;
 
-
 public class Main {
-	public static void main(String[] args) {
-		MainFrame gameFrame = new MainFrame();
-		gameFrame.init();
-	}
+    public static void main(String[] args) {
+        try {
+            MainFrame gameFrame = new MainFrame();
+            gameFrame.init();
+        } catch (Exception e) {
+            System.err.println("Failed to load the application due to: " + e.getMessage());
+        }
+    }
 }
